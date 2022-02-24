c9c1ebc082f378ebb75726514addfb92ee7c9f70
Ali Shafiee <shafiee.ali10@gmail.com>
2022-02-24 09:36:10 +0330

    Solve A - Hit the Lottery problem

---
+++ A - Hit the Lottery.cpp
@@ -0,0 +1,15 @@
+n = int(input())
+counter = 0
+counter += n // 100
+n -= n // 100 * 100
+if n > 0:
+    counter += n // 20
+    n -= n // 20 * 20
+if n > 0:
+    counter += n // 10
+    n -= n // 10 * 10
+if n > 0:
+    counter += n // 5
+    n -= n // 5 * 5
+counter += n
+print(counter)
